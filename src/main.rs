a5168ff815b3861cfe707a2dd7edf54841137c00
James Whaley <juicy66173@gmail.com>
2018-04-13 19:31:26 +0100

    Add basic structure

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,15 @@
+extern crate chrono;
+extern crate futures;
+extern crate hyper;
+#[macro_use]
+extern crate log;
+extern crate serde;
+#[macro_use]
+extern crate serde_derive;
+extern crate serde_json;
+extern crate serde_xml_rs;
+extern crate tokio;
+
+mod sync;
+
 fn main() {}
